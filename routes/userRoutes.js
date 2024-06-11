ca1f96505b2a0c781da466c09c887d7886c4e2aa
Mohit <mohitsrt123@gmail.com>
2024-06-12 00:31:42 +0530

    modified userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const {verifyToken} = require('../middleware/authMiddleware');
 
 router.get('/profile',verifyToken,(req,res)=>{
-    console.log(req.user.email);
     res.status(200).json({message:"Logged In"});
 })
 
